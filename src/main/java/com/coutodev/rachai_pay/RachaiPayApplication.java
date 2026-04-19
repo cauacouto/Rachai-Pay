4d6faf20f3ea6e4362a5c8fdf07bc589bd168744
cauacouto <cauacouto022@gmail.com>
2026-04-18 21:56:12 -0300

    class inicial do projeto

---
+++ src/main/java/com/coutodev/rachai_pay/RachaiPayApplication.java
@@ -0,0 +1,13 @@
+package com.coutodev.rachai_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RachaiPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RachaiPayApplication.class, args);
+	}
+
+}
