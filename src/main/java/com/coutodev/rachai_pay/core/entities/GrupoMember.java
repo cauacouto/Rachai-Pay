2863c9afa6121198113a86bc16a3aa282c454f8a
cauacouto <cauacouto022@gmail.com>
2026-04-18 21:55:21 -0300

    criar grupoMemberEntity

---
+++ src/main/java/com/coutodev/rachai_pay/core/entities/GrupoMember.java
@@ -0,0 +1,12 @@
+package com.coutodev.rachai_pay.core.entities;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class GrupoMember {
+
+    private Long id;
+    private UUID userId;
+    private Long grupoId;
+    private BigDecimal valor;
+}
