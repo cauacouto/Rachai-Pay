3ee9ae122ef14c3a117b0cbf3307c9447c996279
cauacouto <cauacouto022@gmail.com>
2026-04-18 21:56:23 -0300

    class test

---
+++ src/test/java/com/coutodev/rachai_pay/RachaiPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coutodev.rachai_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RachaiPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
