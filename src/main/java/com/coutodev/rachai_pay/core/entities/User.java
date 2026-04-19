02feac38ae79c0994e5e0f48af0ad0b4394c58cc
cauacouto <cauacouto022@gmail.com>
2026-04-18 21:55:34 -0300

    criar UserEntity

---
+++ src/main/java/com/coutodev/rachai_pay/core/entities/User.java
@@ -0,0 +1,10 @@
+package com.coutodev.rachai_pay.core.entities;
+
+import java.util.UUID;
+
+public class User {
+        private UUID id;
+       private String nome;
+       private String password;
+
+}
