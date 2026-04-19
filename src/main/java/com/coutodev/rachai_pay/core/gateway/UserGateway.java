6e00df47170a1be1d2748243f92c21d4f72f0c15
cauacouto <cauacouto022@gmail.com>
2026-04-19 12:59:33 -0300

    feat:criar UserGateway

--- src/main/java/com/coutodev/rachai_pay/core/gateway/UserGateway.java
+++ src/main/java/com/coutodev/rachai_pay/core/gateway/UserGateway.java
@@ -1,13 +1,13 @@
 package com.coutodev.rachai_pay.core.gateway;
 
-import com.coutodev.rachai_pay.core.entities.User;
+import com.coutodev.rachai_pay.core.entities.UserModel;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserGateway {
 
-    User save(User user);
-    Optional<User> findByIdd(UUID id);
+    UserModel save(UserModel userModel);
+    Optional<UserModel> findByIdd(UUID id);
 
 }
